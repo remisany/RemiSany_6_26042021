e750d80fb99c6c96c48b81f422f77b17e51ecc1b
remisany <remi.d.sany@gmail.com>
2021-04-28 19:27:31 +0200

    Ajout fonction filtre

--- index.js
+++ index.js
@@ -1,85 +1,157 @@
-var section = document.querySelector('section');
+fetch("FishEyeDataFR.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        let photographers = json['photographers'];
+        initialize(photographers);
+    })
+    .catch(function(err) {
+        console.log('Fetch problem: ' + err.message);
+    });
 
-//URL fichier JSON
-var requestURL = 'FishEyeDataFR.json';
 
-//Créer requête
-var request = new XMLHttpRequest();
+function initialize(photographers) {
+    const section = document.querySelector('section');
 
-//Ouvrir requête
-request.open('GET', requestURL);
+    let tagsGroup = photographers;
 
-//Type requête + envoi
-request.responseType = 'json';
-request.send();
+    update();
 
-//Réponse requête
-request.onload = function() {
-    var photographersdata = request.response;
-    photographersprofile(photographersdata);
-}
+    function update() {
+        while (section.firstChild) {
+            section.removeChild(section.firstChild);
+        }
 
-function photographersprofile(jsonObj) {
-    var photographers = jsonObj['photographers'];
+        for(let i = 0; i < tagsGroup.length; i++) {
+            showPhotographer(tagsGroup[i]);
+        }
+    }
 
-    for (var i = 0; i < photographers.length; i++) {
+    function showPhotographer(photographer) {
         //article
-        var article = document.createElement('article');
+        const article = document.createElement('article');
         article.setAttribute("class", "photographer");
-
+    
         //photographer-link
-        var a1 = document.createElement('a');
+        const a1 = document.createElement('a');
         a1.setAttribute("class", "photographer-link");
-        var img = document.createElement('img');
+        const img = document.createElement('img');
         img.setAttribute("class", "portrait");
-        var h2 = document.createElement('h2');
+        const h2 = document.createElement('h2');
         h2.setAttribute("class", "name");
-
-        img.src = "Images/Photographers ID Photos/" + photographers[i].portrait;
-        h2.textContent = photographers[i].name;
-
+    
+        img.src = "Images/Photographers ID Photos/" + photographer.portrait;
+        h2.textContent = photographer.name;
+    
         a1.appendChild(img);
         a1.appendChild(h2);
         article.appendChild(a1);
-
+    
         //photographer-infos
-        var div = document.createElement('div');
+        const div = document.createElement('div');
         div.setAttribute("class", "photographer-infos");
-        var p1 = document.createElement('p');
+        const p1 = document.createElement('p');
         p1.setAttribute("class", "citycountry");
-        var p2 = document.createElement('p');
+        const p2 = document.createElement('p');
         p2.setAttribute("class", "tagline");
-        var p3 = document.createElement('p');
+        const p3 = document.createElement('p');
         p3.setAttribute("class", "price");
-
-        p1.textContent = photographers[i].city + ", " + photographers[i].country;
-        p2.textContent = photographers[i].tagline;
-        p3.textContent = photographers[i].price + "€/jour";
-
+    
+        p1.textContent = photographer.city + ", " + photographer.country;
+        p2.textContent = photographer.tagline;
+        p3.textContent = photographer.price + "€/jour";
+    
         div.appendChild(p1);
         div.appendChild(p2);
         div.appendChild(p3);
         article.appendChild(div);
-
+    
         //photographer-tags
-        var nav = document.createElement('nav');
+        const nav = document.createElement('nav');
         nav.setAttribute("class", "photographer-tags");
-        var ul = document.createElement('ul');
+        const ul = document.createElement('ul');
         ul.setAttribute("class", "tags-container");
-
-        var tags = photographers[i].tags;
-        for (var j = 0; j < tags.length; j++) {
-            var a2 = document.createElement('a');
+    
+        let tags = photographer.tags;
+        for (let i = 0; i < tags.length; i++) {
+            let a2 = document.createElement('a');
             a2.setAttribute("class", "tags");
-            var li = document.createElement('li');
-            a2.textContent = "#" + tags[j];
+            let li = document.createElement('li');
+            a2.textContent = "#" + tags[i];
             li.appendChild(a2);
             ul.appendChild(li);
         }
-
+    
         nav.appendChild(ul);
         article.appendChild(nav);
-
+    
         section.appendChild(article);
     }
+
+    const selectTag = document.getElementsByClassName("tags");
+    console.log(selectTag);
+
+    let tagsearch;
+
+    document.getElementById("portrait").onclick = function() {
+        tagsearch = "portrait";
+        search();
+    }
+
+    document.getElementById("art").onclick = function() {
+        tagsearch = "art";
+        search();
+    }
+
+    document.getElementById("fashion").onclick = function() {
+        tagsearch = "fashion";
+        search();
+    }
+
+    document.getElementById("architecture").onclick = function() {
+        tagsearch = "architecture";
+        search();
+    }
+
+    document.getElementById("travel").onclick = function() {
+        tagsearch = "travel";
+        search();
+    }
+
+    document.getElementById("sport").onclick = function() {
+        tagsearch = "sport";
+        search();
+    }
+
+    document.getElementById("animals").onclick = function() {
+        tagsearch = "animals";
+        search();
+    }
+
+    document.getElementById("events").onclick = function() {
+        tagsearch = "events";
+        search();
+    }
+
+    document.getElementById("home").onclick = function() {
+        tagsGroup = photographers;
+        update();
+    }
+
+    function search () {
+        tagsGroup = [];
+        for (let i = 0; i < photographers.length; i++) {
+            let tags = photographers[i].tags;
+            let tagsfilters = tags.filter(function(tag){
+                if (tag === tagsearch) {
+                    tagsGroup.push(photographers[i]);
+                }
+            });
+        }
+        update();
+    }
+
+    tagsGroup = photographers;
+    update();
 }
