622e5cae41d57dc6517699d193c52159896da513
remisany <remi.d.sany@gmail.com>
2021-05-11 10:36:04 +0200

    MAJ compteur de likes

--- likes.js
+++
@@ -1,7 +0,0 @@
-const link = document.querySelectorAll(".medias-photographer__infos__likes");
-
-for (let i = 0; i < link.length; i++) {
-    link[i].addEventListener("click", function() {
-        this.firstChild.textContent = parseInt(this.firstChild.textContent) + 1;
-    });
-}
